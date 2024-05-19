d6fec04479c68b1100801a16cc316facc2e52138
Priytesh Prasad <priyteshprasd@gmail.com>
2024-05-19 09:59:56 +0530

    sol 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  function updateCount() {
+    setCount(count + 1);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <p>Button clicked {count} times</p>
+      <button onClick={updateCount}>Click me</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
